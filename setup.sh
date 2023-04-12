47097d64c796bcb682d1c8a59ba67ac3fe07278c
alekswael <amw98@outlook.dk>
2023-04-12 09:42:21 +0200

    yeash

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+<<<<<<< HEAD
+python -m pip install -r requirements.txt
+python -m spacy download en_core_web_md
+=======
+# install hdbscan for BERTopic
+sudo apt-get update
+sudo apt-get install python3-dev
+
+# requirements
+pip install --upgrade pip
+pip install --upgrade nbformat
+python3 -m pip install -r requirements.txt
+>>>>>>> 52da4eb463d16bfca02f995c5dad9056e2bfb02c
